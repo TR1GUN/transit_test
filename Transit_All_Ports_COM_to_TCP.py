4315299bf25fac8441923951922f7628fd58a8e7
n.buslin <aka_gto@mail.ru>
2021-09-27 19:49:01 +0300

    - начал Рефакторинг кода с наследованием

--- Transit_All_Ports_COM_to_TCP.py
+++ Transit_All_Ports_COM_to_TCP.py
@@ -2,7 +2,7 @@
 import time
 from Service.Setup import Setup
 import threading
-
+from Transit_COM_to_TCP import COMtoTCP
 
 # --------------------------------------------------------------------------------------------------------------------
 # --------------------------------------------------------------------------------------------------------------------
@@ -11,149 +11,12 @@ import threading
 # --------------------------------------------------------------------------------------------------------------------
 
 
-class COMtoTCP(Setup):
+class AllCOMtoTCP(COMtoTCP):
     """
     Это основной класс запуска для способа транзита ТСР на СОМ
     """
-    data = b'\x33\x33\x33\x33\x33'
 
-    RunUp_ports_bool = {}
-    RunUp_ports_name = {}
 
-    setup_command = False
-    Server_dict = {}
-
-    Baudrate = 0
-    ip_address = 'localhost'
-    ip_port_all_dict = {}
-    ip_port = 0
-
-    COM_port = ''
-
-    answer = {}
-
-    def __init__(self, data: str = '33333'):
-
-        # self.data = data
-        self.setup_command = False
-        self.Server_dict = {}
-        # Первое что деалем - парсим наши переменные
-        self._definition_settings()
-        # Второе что делаем - СМОТРИМ наши КОМ ПОРТЫ
-        self._definition_COM_Ports()
-
-        # Теперь переводим нашу войну и мир - Сначала в строку
-        if type(data) != bytes:
-            data = str(data)
-            # и теперь в байты
-            data = data.encode()
-        self.data = data
-
-    def _Setup_received_data_TCP(self, ip_port):
-        """
-        Здесь запускаем данные по TCP IP по определенному порту
-
-        Запускается в отдельном потоке
-
-        :return:
-        """
-
-        from Service.Connect_To_Server import ConnectToSocket
-
-        # send_data = self.data
-        ip_address = str(self.ip_address)
-        # ip_port = int(self.ip_port)
-
-        server = ConnectToSocket(address=(ip_address, ip_port))
-
-        # server.Send_Data(data=send_data)
-        ReceivedPort_data = server.Received_Data()
-        server.Close_socket()
-
-        self.answer[ip_port] = ReceivedPort_data
-
-    def _Setup_send_data_COM(self):
-        """
-        Здесь слушаем COM порт
-
-        Запускается в отдельном потоке
-
-        :return:
-        """
-
-        from Service.Connect_To_RS import ConnectToSerialPort
-
-        send_data = self.data
-
-        Baudrate = int(self.Baudrate)
-
-        COM_port_name = str(self.COM_port)
-
-        # Открываем порт
-        SerialPort = ConnectToSerialPort(Port_Name=COM_port_name, Baudrate_Port=Baudrate)
-
-        # Не пускаем команду пока не разрешим
-        while True:
-            if self.setup_command:
-                # Теперь пускаем команду
-                time.sleep(1)
-                print('Отправляем')
-                SerialPort.Write(data=send_data)
-                break
-        # И закрываем соединение
-        SerialPort.Close()
-
-        time.sleep(1)
-        # отпускаем обмен
-        self.setup_command = False
-
-    def _Setup_server_TCP(self, ip_port):
-        """
-        Здесь запускаем данные по TCP IP по определенному порту
-
-        Запускается в отдельном потоке
-
-        :return:
-        """
-
-        from Service.Connect_To_Server import ConnectToSocket
-
-        ip_address = str(self.ip_address)
-
-        self.Server_dict[ip_port] = ConnectToSocket(address=(ip_address, ip_port)).get_socket()
-
-    def _Received_data_TCP(self, COM_port_name: str):
-
-        """
-        Здесь реализуем чтение с нашего СОМ порта
-
-        """
-
-        Server = self.Server_dict[COM_port_name]
-
-        data = b''
-        while True:
-            # Ожидаем запуска команды
-            if self.setup_command:
-                print('читаем порт')
-                # Работаем пока включена передача
-                while self.setup_command:
-
-                    try:
-                        chack = Server.recv(1)
-
-                        data = data + chack
-
-                    except:
-                        print('Не удалось считать')
-                        break
-
-                break
-        self.answer[COM_port_name] = data
-
-        Server.close()
-
-    # -------------------
 
     def Setup(self, COM: str = 'COM1'):
         """
@@ -238,4 +101,4 @@ class COMtoTCP(Setup):
 
 
 
-# COMtoTCP('gfdfdfdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddf').Setup(COM='COM2')
+AllCOMtoTCP('gfdfdfdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddf').Setup(COM='COM2')
